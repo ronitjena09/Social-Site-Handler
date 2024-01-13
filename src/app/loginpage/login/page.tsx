dd8d51b347ecebb147bac7ee6381767c80a99c93
Ronit Jena <ronitjena64@gmail.com>
2024-01-13 18:27:12 +0530

    login+signup

--- src/app/loginpage/login/page.tsx
+++ src/app/loginpage/login/page.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 
-const page = () => {
+const SignIn = () => {
   return (
-    <div className='flex bg-[#94a3b8] min-h-screen justify-center items-center'>
-      <div className='bg-white p-10 rounded-lg shadow-lg'>
+    <div className='flex bg-[#e5e7eb] min-h-screen justify-center items-center '>
+      <div className='bg-white p-10 rounded-lg shadow-lg '>
         <h1 className='font-bold'> Sign in </h1>
         <h3> to access social</h3>
+
         <div className='flex flex-col my-4'>
           <label></label>
-          <input type='text' placeholder='Email address' className='border-2 outline-none border-zinc-600 rounded-md px-2 py-1'></input>
+          <input type='email' placeholder='Email address' className='border-2 outline-none border-zinc-600 rounded-md px-2 py-1'></input>
         </div>
+        <div className='flex flex-col my-4'>
+          <label></label>
+          <input type='password' placeholder='Password' className='border-2 outline-none border-zinc-600 rounded-md px-2 py-1'></input>
+        </div>
+        <button className='bg-[#06b6d4] w-full py-1 my-2 rounded-md text-white'>Login</button>
       </div>
 
     </div>
   )
 }
 
-export default page
+export default SignIn
--- src/app/loginpage/signup/page.tsx
+++ src/app/loginpage/signup/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import React, { useState } from "react";
 import Balancer from "react-wrap-balancer";
@@ -6,119 +7,91 @@ import linkedin from "/public/images/linkedin.svg";
 import facebook from "/public/images/facebook.svg";
 import google from "/public/images/google.svg";
 
-const Hero = () => {
-  const [companyName, setCompanyName] = useState("");
-  const [url, setUrl] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const [agree, setAgree] = useState("");
-
-  const [companyNameError, setCompanyNameError] = useState("");
-  const [urlError, setUrlError] = useState("");
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
-  const [phoneError, setPhoneError] = useState("");
-  const [agreeError, setAgreeError] = useState("");
-  const validate = () => {
-    let isValid = true;
-
-    // Checking  the company name,url,email,pass,phonenum,agreement is empty or not
-    if (!companyName) {
-      setCompanyNameError("Company name is required");
-      isValid = false;
-    } else {
-      setCompanyNameError("");
-    }
-
-    if (!url) {
-      setUrlError("URL is required");
-      isValid = false;
-    } else if (
-      !/^(https?:\/\/)?[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(
-        url
-      )
-    ) {
-      setUrlError("URL is invalid");
-      isValid = false;
-    } else {
-      setUrlError("");
-    }
-
-    if (!email) {
-      setEmailError("Email is required");
-      isValid = false;
-    } else if (!/\S+@\S+\.\S+/.test(email)) {
-      setEmailError("Email is invalid");
-      isValid = false;
-    } else {
-      setEmailError("");
-    }
-
-    if (!password) {
-      setPasswordError("Password is required");
-      isValid = false;
-    } else if (password.length < 8) {
-      setPasswordError("Password must be at least 8 characters long");
-      isValid = false;
-    } else {
-      setPasswordError("");
-    }
-
-    if (!phone) {
-      setPhoneError("Phone number is required");
-      isValid = false;
-    } else if (!/^\d{10}$/.test(phone)) {
-      setPhoneError("Phone number must be 10 digits");
-      isValid = false;
-    } else {
-      setPhoneError("");
-    }
-
-    if (!agree) {
-      setAgreeError("You must agree to the terms and privacy policy");
-      isValid = false;
-    } else {
-      setAgreeError("");
-    }
-
-    return isValid;
-  };
-
-  const handleSubmit = (e) => {
-    //handle the form submission
-
-    e.preventDefault(); // Prevent the default browser behavior
-
-    if (validate()) {
-      // Validate the form inputs
-
-      console.log("Form submitted successfully"); // If the form is valid, sending the data to the backend
-      console.log(companyName, url, email, password, phone, agree);
-    }
-  };
+const SignIn = () => {
+    const [companyName, setCompanyName] = useState("");
+    const [url, setUrl] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phone, setPhone] = useState("");
+    const [agree, setAgree] = useState("");
+  
+    const [companyNameError, setCompanyNameError] = useState("");
+    const [urlError, setUrlError] = useState("");
+    const [emailError, setEmailError] = useState("");
+    const [passwordError, setPasswordError] = useState("");
+    const [phoneError, setPhoneError] = useState("");
+    const [agreeError, setAgreeError] = useState("");
+    const validate = () => {
+      let isValid = true;
+  
+      // Checking  the company name,url,email,pass,phonenum,agreement is empty or not
+      if (!companyName) {
+        setCompanyNameError("Company name is required");
+        isValid = false;
+      } else {
+        setCompanyNameError("");
+      }
+  
+      if (!url) {
+        setUrlError("URL is required");
+        isValid = false;
+      } else if (
+        !/^(https?:\/\/)?[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i.test(
+          url
+        )
+      ) {
+        setUrlError("URL is invalid");
+        isValid = false;
+      } else {
+        setUrlError("");
+      }
+  
+      if (!email) {
+        setEmailError("Email is required");
+        isValid = false;
+      } else if (!/\S+@\S+\.\S+/.test(email)) {
+        setEmailError("Email is invalid");
+        isValid = false;
+      } else {
+        setEmailError("");
+      }
+  
+      if (!password) {
+        setPasswordError("Password is required");
+        isValid = false;
+      } else if (password.length < 8) {
+        setPasswordError("Password must be at least 8 characters long");
+        isValid = false;
+      } else {
+        setPasswordError("");
+      }
+  
+      if (!phone) {
+        setPhoneError("Phone number is required");
+        isValid = false;
+      } else if (!/^\d{10}$/.test(phone)) {
+        setPhoneError("Phone number must be 10 digits");
+        isValid = false;
+      } else {
+        setPhoneError("");
+      }
+  
+      if (!agree) {
+        setAgreeError("You must agree to the terms and privacy policy");
+        isValid = false;
+      } else {
+        setAgreeError("");
+      }
+  
+      return isValid;
+    };
 
   return (
-    <div>
-      <section className="mx-auto flex max-w-6xl flex-col-reverse gap-2 px-4 pb-12 transition-all md:flex-row md gap-4">
-        {/* left div */}
-        <div className=" flex flex-col gap-6 pt-8 text-center md:w-1/2 md:items-start md:gap-10 md:pt-26 md:text-left">
-          <h1 className="text-4xl font-semibold md:text-5xl">
-            The easiest way to manage your brands on social media
-          </h1>
-          <Balancer>
-            <h4 className="text-lg md:text:2xl">
-              Schedule unlimited posts, monitor what matters, and create
-              custom-reports to analyze your social media performance with Zoho
-              Social.
-            </h4>
-          </Balancer>
-        </div>
-        {/* right div */}
-        <section className="flex  gap-6 pb-2 justify-end items-center md:w-1/2 md:items-start md:gap-10 md:pt-22 md:text-left">
-          <Balancer>
+    <div className=' bg-[#e5e7eb] justify-center items-center '>
+      <div className='bg-white p-10 rounded-lg shadow-lg '>
+      
             <div className="p-6 pb-48 justify-center items-center h-screen flex">
-              <form className="flex flex-col w-66" onSubmit={handleSubmit}>
+              <form className="flex flex-col w-66" >
                 <h5 className="text-lg font-semibold mb-4">
                   Get started in 30 seconds
                 </h5>
@@ -199,8 +172,7 @@ const Hero = () => {
                     id="agree"
                     type="checkbox"
                     aria-label="agree to terms and privacy policy"
-                    checked={agree}
-                    onChange={(e) => setAgree(e.target.checked)}
+                    
                   />
                   <label htmlFor="agree">
                     I agree to the{" "}
@@ -262,11 +234,10 @@ const Hero = () => {
                 </div>
               </form>
             </div>
-          </Balancer>
-        </section>
-      </section>
-    </div>
-  );
-};
+      </div>
 
-export default Hero;
+    </div>
+  )
+}
+
+export default SignIn
